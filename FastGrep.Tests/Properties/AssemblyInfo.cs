ab89a8c5e9b47bfddc0b0e9f0d97c6e295a3017b
Raif Atef <beliskner.github.3psil0N@neverbox.com>
2012-07-20 16:21:06 +0200

    - Initial project commit.

---
+++ FastGrep.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FastGrep.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("n7Epsilon")]
+[assembly: AssemblyProduct("FastGrep.Tests")]
+[assembly: AssemblyCopyright("Copyright © n7Epsilon 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b2875a4f-0b66-41a8-8bb1-8c1e8883f4a7")]
+
