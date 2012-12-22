c119fbeeb3a073898fc512b56bbc8adf1c10b15f
epsilon <epsilon@localhost>
2012-12-22 22:23:02 +0200

    - Disable resharper comments about multiple enumeration. Add a safety check. - Make setters private.

--- FastGrep.Engine/MatchFoundEventArgs.cs
+++ FastGrep.Engine/MatchFoundEventArgs.cs
@@ -1,21 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using EnsureThat;
 
 namespace FastGrep.Engine
 {
     public class MatchFoundEventArgs : EventArgs
     {
-        public string FilePath { get; set; }
-        public IEnumerable<MatchedLine> Matches { get; set; }
+        public string FilePath { get; private set; }
+        public IEnumerable<MatchedLine> Matches { get; private set; }
 
         public MatchFoundEventArgs(string filePath, IEnumerable<MatchedLine> matches)
         {
             Ensure.That(filePath, "filePath").IsNotNullOrWhiteSpace();
+            // ReSharper disable PossibleMultipleEnumeration
             Ensure.That(matches, "matches").IsNotNull();
 
+            var matchedLines = matches as IList<MatchedLine> ?? matches.ToList();
+            // ReSharper restore PossibleMultipleEnumeration
+
             this.FilePath = filePath;
-            this.Matches = matches;
+            this.Matches = matchedLines;
         }
     }
 }
