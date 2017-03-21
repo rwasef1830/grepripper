1255a5afb2a01c3e00b7ae48ffbe1195956dcc25
epsilon <epsilon@localhost>
2017-03-21 23:22:15 +0200

    Update to VS2017 and retarget to .NET 4.5

--- FunkyGrep.UI/Properties/AssemblyInfo.cs
+++ FunkyGrep.UI/Properties/AssemblyInfo.cs
@@ -35,7 +35,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Raif Atef Wasef")]
 [assembly: AssemblyProduct("FunkyGrep")]
-[assembly: AssemblyCopyright("Copyright © Raif Atef Wasef 2012")]
+[assembly: AssemblyCopyright("Copyright © Raif Atef Wasef 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
