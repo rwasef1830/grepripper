b74cb1ecf3a66dc949ce3d8070175b23763f9ed5
Raif Atef <beliskner.github.3psil0N@neverbox.com>
2020-04-30 21:33:32 +0200

    Focus pattern textbox after search completion.

---
+++ FunkyGrep.UI/Util/FocusUtil.cs
@@ -0,0 +1,62 @@
+﻿#region License
+// Copyright (c) 2020 Raif Atef Wasef
+// This source file is licensed under the  MIT license.
+// 
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+// KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+// WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
+// OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace FunkyGrep.UI.Util
+{
+    public static class FocusUtil
+    {
+        public static readonly DependencyProperty IsFocusedProperty =
+            DependencyProperty.RegisterAttached(
+                "IsFocused",
+                typeof(bool),
+                typeof(FocusUtil),
+                new UIPropertyMetadata(false, null, OnIsFocusedCoerceValue));
+
+        public static bool GetIsFocused(DependencyObject obj)
+        {
+            return (bool) obj.GetValue(IsFocusedProperty);
+        }
+
+        public static void SetIsFocused(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsFocusedProperty, value);
+        }
+
+        static object OnIsFocusedCoerceValue(DependencyObject d, object baseValue)
+        {
+            var uie = (UIElement)d;
+            if (!(bool)baseValue)
+            {
+                return false;
+            }
+
+            uie.Focus();
+            Keyboard.Focus(uie);
+            return true;
+        }
+    }
+}
